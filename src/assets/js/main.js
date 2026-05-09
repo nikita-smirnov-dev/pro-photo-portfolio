8b43f70b988e55e14149b8e036b9add2a82440c2
Nikita Smirnov <nikita_smirnov91@mail.ru>
2026-05-09 17:32:47 +0300

    added states to links and image switching

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,7 @@
 import { getHeaderheight } from '../js/helpers/header-height';
+import { initFancybox } from './components/project-gallery-fancy';
 
 document.addEventListener('astro:page-load', () => {
   getHeaderheight();
+  initFancybox();
 });

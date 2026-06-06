f8d794c0e278d8ded09f309d724439b4e4a970c9
Nikita Smirnov <nikita_smirnov91@mail.ru>
2026-06-06 18:04:40 +0300

    added button go back for all projects and added favicon

--- src/assets/ts/components/project-gallery-fancy.ts
+++ src/assets/ts/components/project-gallery-fancy.ts
@@ -4,16 +4,7 @@ export function initFancybox() {
   Fancybox.bind('[data-fancybox="gallery"]', {
     showClass: 'f-fadeIn',
     hideClass: 'f-fadeOut',
-    mainStyle: {
-      '--f-toolbar-padding': '16px 32px',
-      '--f-toolbar-gap': '8px',
-      '--f-button-border-radius': '50%',
-      '--f-thumb-width': '82px',
-      '--f-thumb-height': '82px',
-      '--f-thumb-opacity': '0.5',
-      '--f-thumb-hover-opacity': '1',
-      '--f-thumb-selected-opacity': '1',
-    },
+
     Carousel: {
       transition: 'slide',
       Toolbar: {
